65f4011b6df601993c809c9476bfe1cdf1ba8fb0
565Vivek <vivekputta@berkeley.edu>
2024-10-19 23:50:54 -0700

    Marker v1

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,6 +1,7 @@
-// frontend/src/components/Map.js
 import React from 'react';
-import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import MovingMarker from './MovingMarker'; // Import the MovingMarker component
+import 'leaflet/dist/leaflet.css';
 
 function Map({ hurricanes, vehicles }) {
   const position = [25.7617, -80.1918]; // Center on Miami
@@ -13,35 +14,30 @@ function Map({ hurricanes, vehicles }) {
       />
       {hurricanes && hurricanes.map(hurricane => (
         <React.Fragment key={hurricane.id}>
-            {hurricane.forecasted_path && hurricane.forecasted_path.length > 0 && (
-                <Polyline
-                    positions={hurricane.forecasted_path.map(coord => [coord.latitude, coord.longitude])}
-                    color="red"
-                />
-    )}
-    {hurricane.past_path && hurricane.past_path.length > 0 && (
-      <Polyline
-        positions={hurricane.past_path.map(coord => [coord.latitude, coord.longitude])}
-        color="orange"
-      />
-    )}
-  </React.Fragment>
-))}
+          {hurricane.forecasted_path && hurricane.forecasted_path.length > 0 && (
+            <Polyline
+              positions={hurricane.forecasted_path.map(coord => [coord.latitude, coord.longitude])}
+              color="red"
+            />
+          )}
+          {hurricane.past_path && hurricane.past_path.length > 0 && (
+            <Polyline
+              positions={hurricane.past_path.map(coord => [coord.latitude, coord.longitude])}
+              color="orange"
+            />
+          )}
+        </React.Fragment>
+      ))}
 
       {vehicles && vehicles.map(vehicle => (
-        <React.Fragment key={vehicle.id}>
-          <Polyline
-            positions={vehicle.route.map(coord => [coord.latitude, coord.longitude])}
-            color="blue"
-          />
-          <Marker
-            position={vehicle.current_location.split(',').map(Number)}
-          >
-            <Popup>
-              {vehicle.type} en route to {vehicle.destination}
-            </Popup>
-          </Marker>
-        </React.Fragment>
+        <MovingMarker
+          key={vehicle.id}
+          coords={vehicle.route}              // Pass the route coordinates
+          iconUrl='/car-icon.png'            // Pass the icon URL (e.g., car image)
+          type={vehicle.type}                // Pass the type of vehicle
+          popupText={`en route to ${vehicle.destination}`} // Customize the popup text
+          intervalMs={1000}                  // Customize the interval for animation if needed
+        />
       ))}
     </MapContainer>
   );
